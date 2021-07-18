194dd948e677fc4016ec0c146ae7667efaf89cd3
Adaspeaks <adaspeaks@gmail.com>
2021-07-17 18:49:05 -0700

    added questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,61 @@ const answerA = document.querySelector("#answerA");
 const answerA = document.querySelector("#answerB");
 const answerA = document.querySelector("#answerC");
 const answerA = document.querySelector("#answerD");
+
+function viewHighscore(){
+    
+
+};
+
+const questions = [
+    {
+        question:"Choose the 3rd option",
+        options:{answerA:"A: 3rd",answerB:"B: 3rd",answerC:"C: actually 3rd",answerD:"D: 4th"},
+        correct: "answerC"
+    },
+    {
+        question:"the answer is C",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    },
+    {
+        question:"Like a standardized test, the answer is still C",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    },
+    {
+        question:"I lied, the answer is D now",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerD"
+    },
+    {
+        question:"QUICK! What was the answer to the first question!?",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    },
+    {
+        question:"See I told you the answer was always C",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    },
+    {
+        question:"Allthough doing this may actually develop a pavlov response...",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    },
+    {
+        question:"And that may not be good...",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    },
+    {
+        question:"you know let's break the pattern choose A.",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    },
+    {
+        question:"If you fell for it, I'm dissapointed and ashamed... choose C",
+        options:{answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+        correct: "answerC"
+    }
+];
